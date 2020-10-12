91411d9319230976274b99b06cb5880a22e67601
Sou00 <69080602+Sou00@users.noreply.github.com>
2020-10-12 23:39:16 +0200

    Update main.cpp

--- Zadanie 1/main.cpp
+++ Zadanie 1/main.cpp
@@ -1,55 +1,7 @@
-#include "header.h"
+#include "InsertionSort.h"
 
 using namespace std;
 
-class InsertionSort
-{
-public:
-    int* a;
-    int n;
-
-public:
-    InsertionSort(int length, int b[])
-    {
-        this->a = b;
-        this->n = length;
-    }
-    void sort()
-    {
-        int elem;
-        for(int j = 1,i ;j < n; j++)
-        {
-            elem = a[j];
-            i = j-1;
-            while(i >= 0 && a[i] > elem)
-            {
-                a[i+1] = a[i];
-                i--;
-            }
-            a[i+1] = elem;
-        }
-    }
-
-    void print()
-    {
-        bool sorted = true;
-        for(int i=0; i < n; i++)
-        {
-            if(i > 0)
-            {
-                if(a[i] < a[i-1])
-                    sorted=false;
-            }
-            cout << a[i] <<" ";
-        }
-        cout << endl;
-        if(sorted)
-        {
-            cout << "Tablica posortowana" << endl;
-        }
-        else cout << "Tablica nieposortowana" << endl;
-    }
-};
 
 int main() {
     int tab[]= {9,8,7,6,5,4,3,2,1};
