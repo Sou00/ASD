15839b622e4693c94a8a50aa8645471d783403db
Sou00 <69080602+Sou00@users.noreply.github.com>
2020-11-03 17:49:34 +0100

    Add files via upload

---
+++ Sortowanie/InsertionSort.h
@@ -0,0 +1,22 @@
+//
+// Created by Sou on 03.11.2020.
+//
+
+#ifndef ASD_SORTOWANIE_INSERTIONSORT_H
+#define ASD_SORTOWANIE_INSERTIONSORT_H
+
+
+class InsertionSort
+{
+public:
+    int* a = new int [0];
+    int n = 0;
+
+public:
+    InsertionSort(int length, int b[]);
+    void sort();
+    void print();
+};
+
+
+#endif //ASD_SORTOWANIE_INSERTIONSORT_H
